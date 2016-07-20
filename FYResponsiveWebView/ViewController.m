3d8c55509b7ac0d0ffa6e3ad86e8bbb6e0c052f9
Farhan Yousuf <farhanyousuf.official@gmail.com>
2016-07-20 20:35:56 +0530

    Adding Resources
    Resources for the project

--- FYResponsiveWebView/ViewController.m
+++ FYResponsiveWebView/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -17,11 +19,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *localURL = [[NSBundle mainBundle] pathForResource:@"index_inline" ofType:@"html"];
+    NSError *error;
+    NSString *content = [NSString stringWithContentsOfFile:localURL encoding:NSUTF8StringEncoding error:&error];
+//    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:localURL]];
+    [_webView loadHTMLString:content baseURL:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    
+    NSURL *URL = [request URL];
+    NSLog(@"%@",[URL absoluteString]);
+    if ([[URL scheme] isEqualToString:@"applewebdata"]) {
+        NSString *urlString = [[request URL] absoluteString];
+        NSArray *urlParts = [urlString componentsSeparatedByString:@":"];
+        //check to see if we just got the scheme
+        if ([urlParts count] > 1) {
+            NSArray *parameters = [[urlParts objectAtIndex:1] componentsSeparatedByString:@"#"];
+            NSString *variableName = [parameters objectAtIndex:1];
+            
+            NSString *message = [NSString stringWithFormat:@"Button Clicked = %@",variableName];
+            
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Great!" message:message preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                
+                [self dismissViewControllerAnimated:YES completion:nil];
+                
+            }]];
+            [self presentViewController:alert animated:YES completion:nil];
+            
+//            [self.webView stringByEvaluatingJavaScriptFromString:@"alert('.cal_events li a');"];
+
+        }
+    }
+    
+    return YES;
 }
 
 @end
